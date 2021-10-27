5134e139e9d368bd645acd5fad58f2fcf57b4b3c
Khaled Abo <khaledabo@Khaleds-MacBook-Pro.local>
2021-10-27 20:17:24 +0200

    sorting functions according to MVC

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,11 +1,10 @@
+/**
+ *
+ * Model Logic
+ *
+ */
+//#region
 const state = {};
-
-main();
-
-function main() {
-  initState();
-  initActions();
-}
 
 function initState() {
   state.products = [];
@@ -13,7 +12,7 @@ function initState() {
 
 function initActions() {
   const buyBtns = document.querySelectorAll("main  article  button");
-  buyBtns.forEach((btn) => btn.addEventListener("click", ckeckHoodieType));
+  buyBtns.forEach((btn) => btn.addEventListener("click", updateStateProducts));
   state.buyBtns = buyBtns;
 
   // Save products list to local storage.
@@ -24,7 +23,7 @@ function initActions() {
   });
 }
 
-function ckeckHoodieType(e) {
+function updateStateProducts(e) {
   const article = e.target.parentElement;
   const hoodieType = article.querySelector("h3").innerText;
   const prodcutPrice = article.querySelector("h5").innerText;
@@ -40,20 +39,80 @@ function ckeckHoodieType(e) {
     quan: 1,
   };
 
-  updateProductsArray(product);
-}
-
-function updateProductsArray(product) {
   const arrayProduct = state.products.find(
     (pro) => pro.hoodieType === product.hoodieType
   );
   arrayProduct ? arrayProduct.quan++ : state.products.push(product);
   updateBuyBtns();
-  deployProductsArray();
+  updateDropDown();
   console.log(state.products);
 }
 
-function deployProductsArray() {
+function createEventListener() {
+  document
+    .querySelectorAll(".popup-cart article > button")
+    .forEach((btn) => btn.addEventListener("click", handleRemoveBtn));
+  document
+    .querySelectorAll(".plus")
+    .forEach((btn) => btn.addEventListener("click", handlePlus));
+  document
+    .querySelectorAll(".minus")
+    .forEach((btn) => btn.addEventListener("click", handleMinus));
+}
+//#endregion
+/**
+ *
+ * Controller Logic
+ *
+ */
+//#region
+
+function handleRemoveBtn(e) {
+  const article = e.target.parentElement;
+  const nameInCart = article.querySelector("span:nth-of-type(1)").innerText;
+  for (let i in state.products) {
+    if (state.products[i].productName == nameInCart) {
+      state.products.splice(i, 1);
+    }
+  }
+  e.target.parentElement.remove();
+  updateBuyBtns();
+}
+
+function handlePlus(e) {
+  const article = e.target.parentElement.parentElement;
+  const nameInCart = article.querySelector("span").innerText;
+  const findProduct = state.products.find(
+    (pro) => pro.productName == nameInCart
+  );
+  findProduct.quan++;
+  updateBuyBtns();
+  updateDropDown();
+}
+
+function handleMinus(e) {
+  const article = e.target.parentElement.parentElement;
+  const nameInCart = article.querySelector("span").innerText;
+  const index = state.products.findIndex(
+    (pro) => pro.productName == nameInCart
+  );
+  state.products[index].quan--;
+  updateBuyBtns();
+  updateDropDown();
+}
+
+//#endregion
+
+/**
+ *
+ * View Logic
+ *
+ */
+
+//#region
+//
+
+function updateDropDown() {
   const popUpDiv = document.querySelector(".popup-cart > div");
   let productsHtml = "";
   for (let product of state.products) {
@@ -80,67 +139,6 @@ function deployProductsArray() {
   createEventListener();
 }
 
-// function showCart() {
-//   const cart = document.querySelector(".popup-cart");
-//   cart.style.display = "block";
-//   setTimeout(() => {
-//     cart.style.display = "none";
-//   }, 1500);
-// }
-
-function createEventListener() {
-  document
-    .querySelectorAll(".popup-cart article > button")
-    .forEach((btn) => btn.addEventListener("click", handleRemoveBtn));
-  document
-    .querySelectorAll(".plus")
-    .forEach((btn) => btn.addEventListener("click", handlePlus));
-  document
-    .querySelectorAll(".minus")
-    .forEach((btn) => btn.addEventListener("click", handleMinus));
-}
-
-function handleRemoveBtn(e) {
-  const article = e.target.parentElement;
-  const nameInCart = article.querySelector("span:nth-of-type(1)").innerText;
-  for (let i in state.products) {
-    if (state.products[i].productName == nameInCart) {
-      state.products.splice(i, 1);
-    }
-  }
-  const names = document.querySelectorAll("main > article > h2");
-  for (let name of names) {
-    if (name.innerText == nameInCart) {
-      const parent = name.parentElement;
-      parent.querySelector("button").innerText = "Buy";
-    }
-  }
-  e.target.parentElement.remove();
-}
-
-function handlePlus(e) {
-  const article = e.target.parentElement.parentElement;
-  const nameInCart = article.querySelector("span").innerText;
-  const findProduct = state.products.find(
-    (pro) => pro.productName == nameInCart
-  );
-  findProduct.quan++;
-  updateBuyBtns();
-  deployProductsArray();
-}
-
-function handleMinus(e) {
-  const article = e.target.parentElement.parentElement;
-  const nameInCart = article.querySelector("span").innerText;
-  const index = state.products.findIndex(
-    (pro) => pro.productName == nameInCart
-  );
-  state.products[index].quan--;
-
-  updateBuyBtns();
-  deployProductsArray();
-}
-
 function updateBuyBtns() {
   for (let btn of state.buyBtns) {
     const parent = btn.parentElement;
@@ -157,6 +155,30 @@ function updateBuyBtns() {
       } else {
         btn.innerText = `${state.products[productIndex].quan} in cart`;
       }
+    } else {
+      btn.innerText = `Buy`;
     }
   }
 }
+
+// function showCart() {
+//   const cart = document.querySelector(".popup-cart");
+//   cart.style.display = "block";
+//   setTimeout(() => {
+//     cart.style.display = "none";
+//   }, 1500);
+// }
+//#endregion
+
+/**
+ *
+ * Entry Point
+ *
+ */
+
+function main() {
+  initState();
+  initActions();
+}
+
+main();
